f2fa4ce1cdbeea2854cb2508b1911823eee9d3ac
BalajiJBcs <jbbalajics@gmail.com>
2020-07-24 07:01:05 +0530

    project: tic tac toe of project
    Signed-off-by: BalajiJBcs <jbbalajics@gmail.com>

--- 08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ 08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+
+let ticTacToe = {
+    board : [
+        [null, null, null],
+        [null, null, null],
+        [null, null,  null]],
+    move: function(va, x, y) {
+        if(!this.board[x][y]) {
+            this.board[x][y] = va;
+            return this.board;
+        }
+        return this.board;
+    },
+    clear: function() {
+
+        let mat = this.board;
+        for(let i = 0; i < mat.length; i++) {
+            for (let j = 0; j < mat[0].length; j++) {
+                this.board[i][j] = null;
+            }
+        }
+
+        return this.board;
+    }
+
+};
