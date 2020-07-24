eaf2dea4745dee1de86581cdefcfeb09ad93544c
BalajiJBcs <jbbalajics@gmail.com>
2020-07-24 06:06:54 +0530

    problem : add support for the my-slice-spec.js
    Signed-off-by: BalajiJBcs <jbbalajics@gmail.com>

--- 05-arrays-i/01-odd-couple/odd-couple.js
+++ 05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+// YOUR CODE BELOW
+function oddCouple(arr) {
+    let resultOdd =[];
+    for(let i = 0; i< arr.length; i++) {
+        if(arr[i] % 2 !=0) {
+            if(resultOdd.length <= 1) {
+                resultOdd.push(arr[i]);
+            }
+        }
+    }
+    return resultOdd;
+}
