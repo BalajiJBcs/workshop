7026be1a9e560cdcea9a447538343f73309124eb
BALAJI.J.B <jbbalajics@gmail.com>
2020-07-29 12:47:52 +0530

    Update finder-function.js

--- 10-higher-order-functions-i/02-finder-function/finder-function.js
+++ 10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+const finderFunction = (number, isEven) => {
+  let index = 0;
+  for(let i = 0; i < number.length;i++) {
+    let val = isEven(number[i]);
+    if(val) {
+      index = i;
+      return index;
+    }
+  }
+  return -1;
+}
