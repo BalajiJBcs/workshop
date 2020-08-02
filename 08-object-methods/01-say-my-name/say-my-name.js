fd0f36e5680f002adbd753f2cb1bcc97a7b5fd09
BalajiJBcs <jbbalajics@gmail.com>
2020-08-02 13:32:37 +0530

    Say your name
    Update the say you name source code

--- 08-object-methods/01-say-my-name/say-my-name.js
+++ 08-object-methods/01-say-my-name/say-my-name.js
@@ -1,1 +1,11 @@
-// YOUR CODE BELOW
+
+
+const constThemAll = (obj, val) => {
+    let arr = [];
+    for (let key in obj) {
+        if (typeof obj[key] === 'function') {
+            let values  = obj[key];
+            arr.push(values())
+        }
+    }
+}
--- 08-object-methods/02-say-your-name/say-your-name.js
+++ 08-object-methods/02-say-your-name/say-your-name.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+// YOUR CODE BELOW
+
+let me2 = {
+    name : 'Kat',
+    getGreeting : function (person) {
+        return `Hi ${person.name}, my name is ${this.name}`
+    }
+}
